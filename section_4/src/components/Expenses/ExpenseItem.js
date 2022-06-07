36a9f87cd84892bd9279f3547d2489064fe67c71
emadomari <emad.alomari@stallion.ai>
2022-06-08 00:59:17 +0400

    add comments to the state

--- section_4/src/components/Expenses/ExpenseItem.js
+++ section_4/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,19 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
+
+//import useState to use it inside the button function
 import React, {useState} from 'react';
 
 let c = 0;
 const ExpenseItem = (props) => {
     
+    //create a list variable for the state, the first element is the current state and the second one the the future state
     const [title, setTitle] = useState(props.title);
 
+    //create a function for changing the state using a button
     const clickHandler =() =>{
+        //here we set the new state value
         setTitle("Title: " + c);
         c++;
         console.log('Clicked');
@@ -21,6 +26,7 @@ const ExpenseItem = (props) => {
                 <div> <h2>{title}</h2></div>
                 <div className="expense-item__price"> ${props.amount}</div>
             </div>
+            {/* create a button and to use the clickHandler function*/}
             <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
